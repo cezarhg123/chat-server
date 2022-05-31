064a268a0b6ef1de6a3ea745b0755a236eafdee5
Cezarhg2007 <cezarhg.89.popo9@gmail.com>
2022-05-31 16:02:40 +0100

    fix

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,9 @@
 use std::{io::{Read, Write}, thread};
 
 fn main() {
-    let listerner = std::net::TcpListener::bind("127.0.0.1:69").unwrap();
+    let mut local_ip = String::new();
+    std::io::stdin().read_line(&mut local_ip).unwrap();
+    let listerner = std::net::TcpListener::bind(local_ip).unwrap();
     let (mut stream1, _remote_address1) = listerner.accept().unwrap();
     let (mut stream2, _remote_address2) = listerner.accept().unwrap();
     let mut stream1_clone = stream1.try_clone().unwrap();
